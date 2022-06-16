b834f6ccc848f447d1dd7e90e3165fc1671e8432
Igor De Sousa <sr.igor.dev@gmail.com>
2022-06-16 13:30:38 -0300

    Inclusão do arquivo Readme.md

--- olxcloneBack/dist/controllers/ApiController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ping = void 0;
-const ping = (req, res) => {
-    res.status(200);
-    res.json({ pong: true });
-};
-exports.ping = ping;
